d36ebc8c44f044470b1f87ec1fa705947d2aaf0d
huangweichang <weichang@bianjie.ai>
2022-07-01 11:20:07 +0800

    close index code

--- src/schema/ibc_tx.schema.ts
+++ src/schema/ibc_tx.schema.ts
@@ -56,8 +56,8 @@ IbcTxSchema.index({substate:-1, status: -1, next_retry_time:-1}, {background: tr
 IbcTxSchema.index({status: -1, tx_time: -1}, {background: true});
 IbcTxSchema.index({sc_chain_id: -1}, {background: true});
 IbcTxSchema.index({dc_chain_id: -1}, {background: true});
-IbcTxSchema.index({'denoms.sc_denom': -1, sc_chain_id: -1}, {background: true});
-IbcTxSchema.index({'denoms.dc_denom': -1, dc_chain_id: -1}, {background: true});
+// IbcTxSchema.index({'denoms.sc_denom': -1, sc_chain_id: -1}, {background: true});
+// IbcTxSchema.index({'denoms.dc_denom': -1, dc_chain_id: -1}, {background: true});
 IbcTxSchema.index({'sc_tx_info.hash': -1}, {background: true});
 IbcTxSchema.index({'dc_tx_info.hash': -1}, {background: true});
 
