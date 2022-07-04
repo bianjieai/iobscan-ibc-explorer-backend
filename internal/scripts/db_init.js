29b88296764795f54e844b25a7a2c296b1471a50
heke <heke@bianjie.ai>
2022-07-04 10:49:29 +0800

    js & task name

--- internal/scripts/db_init.js
+++ internal/scripts/db_init.js
@@ -1,48 +1,72 @@
-db.ibc_chain.createIndex({'chain_id':-1},{background:true,unique:true});
+db.ibc_chain.createIndex({'chain_id': -1}, {background: true, unique: true});
 
 db.ibc_relayer.createIndex({
-    "chain_a" : -1,
-    "channel_a" : -1,
-    "chain_a_address" : -1
-},{background:true,unique:true});
+    "chain_a": -1,
+    "channel_a": -1,
+    "chain_a_address": -1
+}, {background: true, unique: true});
 
 db.ibc_relayer.createIndex({
-    "chain_b" : -1,
-    "channel_b" : -1,
-    "chain_b_address" : -1
-},{background:true,unique:true});
+    "chain_b": -1,
+    "channel_b": -1,
+    "chain_b_address": -1
+}, {background: true, unique: true});
 
 db.ibc_relayer_config.createIndex({
-    "relayer_channel_pair" : -1
-},{background:true,unique:true});
+    "relayer_channel_pair": -1
+}, {background: true, unique: true});
 
 db.ibc_relayer_statistics.createIndex({
-    "transfer_base_denom" : -1,
-    "relayer_id" : -1,
-    "chain_id" : -1,
-    "channel" : -1
-},{background:true,unique:true});
-
+    "transfer_base_denom": 1,
+    "relayer_id": 1,
+    "chain_id": 1,
+    "channel": 1,
+    "segment_start_time": -1,
+    "segment_end_time": -1
+}, {
+    unique: true,
+    background: true
+});
 
 
 db.ibc_channel.createIndex({
     "channel_id": 1
-}, {background:true,unique:true});
+}, {background: true, unique: true});
 
 
 db.ibc_channel_statistics.createIndex({
     "channel_id": 1,
-    "transfer_base_denom":1
-}, {background:true,unique:true});
+    "transfer_base_denom": 1,
+    "segment_start_time": -1,
+    "segment_end_time": -1
+}, {
+    unique: true,
+    background: true
+});
 
 
 db.ibc_token.createIndex({
     "base_denom": 1,
     "chain_id": 1
-}, {background:true,unique:true});
+}, {background: true, unique: true});
 
 
 db.ibc_token_statistics.createIndex({
+    "base_denom": 1,
+    "segment_start_time": -1,
+    "segment_end_time": -1
+}, {
+    unique: true,
+    background: true
+});
+
+
+db.ibc_token_trace_statistics.createIndex({
+    "chain_id": 1,
     "denom": 1,
-    "chain_id": 1
-}, {background:true,unique:true});
+    "segment_start_time": -1,
+    "segment_end_time": -1
+}, {
+    unique: true,
+    background: true
+});
