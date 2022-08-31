730fd08ca3d4e923f3bdece400bb94b3298a3963
huangweichang <weichang@bianjie.ai>
2022-08-31 11:16:22 +0800

    adjust data support api path name

--- internal/app/api/rest/api_support.go
+++ internal/app/api/rest/api_support.go
@@ -18,7 +18,7 @@ type ApiSupportController struct {
 // @Accept  json
 // @Produce  json
 // @Success 200 {object} vo.StatisticInfoResp	"success"
-// @Router /ibc/statistics/api_support [get]
+// @Router /data/statistics/api_support [get]
 func (ctl *ApiSupportController) StatisticInfo(c *gin.Context) {
 	resp, err := staticInfoService.IbcTxStatistic()
 	if err != nil {
@@ -37,7 +37,7 @@ func (ctl *ApiSupportController) StatisticInfo(c *gin.Context) {
 // @Param   page_num    query   int true    "page num" Default(1)
 // @Param   page_size   query   int true    "page size" Default(10)
 // @Success 200 {object} vo.FailTxsListResp	"success"
-// @Router /ibc/fail_txs/api_support [get]
+// @Router /data/fail_txs/api_support [get]
 func (ctl *ApiSupportController) FailTxsList(c *gin.Context) {
 	var req vo.FailTxsListReq
 	if err := c.ShouldBind(&req); err != nil {
@@ -63,7 +63,7 @@ func (ctl *ApiSupportController) FailTxsList(c *gin.Context) {
 // @Param   tx_hash     query   string    false "tx_hash"
 // @Param   chain_id     query   string    false "chain_id"
 // @Success 200 {object} vo.RelayerTxFeesResp	"success"
-// @Router /ibc/relayers_fee/api_support [get]
+// @Router /data/relayers_fee/api_support [get]
 func (ctl *ApiSupportController) RelayerTxsFee(c *gin.Context) {
 	var req vo.RelayerTxFeesReq
 	if err := c.ShouldBind(&req); err != nil {
@@ -85,9 +85,43 @@ func (ctl *ApiSupportController) RelayerTxsFee(c *gin.Context) {
 // @Accept  json
 // @Produce  json
 // @Success 200 {object} vo.AccountsDailyResp	"success"
-// @Router /ibc/accounts_daily/api_support [get]
+// @Router /data/accounts_daily/api_support [get]
 func (ctl *ApiSupportController) AccountsDaily(c *gin.Context) {
 	resp, err := staticInfoService.AccountsDailyStatistic()
+	if err != nil {
+		c.JSON(http.StatusOK, response.FailError(err))
+		return
+	}
+	c.JSON(http.StatusOK, response.Success(resp))
+}
+
+// @Summary list
+// @Description get IBC BUSD of chains
+// @ID list
+// @Tags api_support
+// @Accept  json
+// @Produce  json
+// @Param   page_num    query   int true    "page num" Default(1)
+// @Param   page_size   query   int true    "page size" Default(10)
+// @Param   use_count   query   bool false    "if used count" Enums(true, false)
+// @Success 200 {object} vo.ChainListResp	"success"
+// @Router /data/chainList/api_support [get]
+func (ctl *ApiSupportController) List(c *gin.Context) {
+	var req vo.ChainListReq
+	if err := c.ShouldBind(&req); err != nil {
+		c.JSON(http.StatusOK, response.FailError(errors.Wrap(err)))
+		return
+	}
+	if req.UseCount {
+		total, err := chainService.Count()
+		if err != nil {
+			c.JSON(http.StatusOK, response.FailError(err))
+			return
+		}
+		c.JSON(http.StatusOK, response.Success(total))
+		return
+	}
+	resp, err := chainService.List(&req)
 	if err != nil {
 		c.JSON(http.StatusOK, response.FailError(err))
 		return
