211580c864878ac3dc4e1389208a6f7477e2f446
huangweichang <weichang@bianjie.ai>
2022-09-16 16:35:32 +0800

    delete no use code

--- internal/app/task/ibc_sync_acknowledge_tx_task.go
+++ internal/app/task/ibc_sync_acknowledge_tx_task.go
@@ -124,41 +124,20 @@ func SaveRecvPacketTx(ibcTx *entity.ExIbcTx, history bool) error {
 	if err != nil {
 		return err
 	}
-	//dcAddrMap := make(map[string]struct{}, 20)
-	//if len(recvTxs) > 0 {
-	//	relayers, err := relayerRepo.FindRelayer(ibcTx.ScChainId, ibcTx.RefundedTxInfo.Msg.CommonMsg().Signer, ibcTx.ScChannel)
-	//	if err != nil {
-	//		return err
-	//	}
-	//	for _, val := range relayers {
-	//		if val.ChainAAddress == ibcTx.RefundedTxInfo.Msg.CommonMsg().Signer && val.ChainBAddress != "" {
-	//			dcAddrMap[val.ChainBAddress] = struct{}{}
-	//		} else if val.ChainBAddress == ibcTx.RefundedTxInfo.Msg.CommonMsg().Signer && val.ChainAAddress != "" {
-	//			dcAddrMap[val.ChainAAddress] = struct{}{}
-	//		}
-	//	}
-	//}
 	var recvTx *entity.Tx
 	for _, val := range recvTxs {
 		if val.Status == entity.TxStatusSuccess {
 			recvTx = val
-			for index, msg := range val.DocTxMsgs {
-				if msg.Type == constant.MsgTypeRecvPacket {
-					ibcTx.DcConnectionId = getConnectByRecvPacketEventsNews(val.EventsNew, index)
+			if ibcTx.DcConnectionId == "" {
+				for index, msg := range val.DocTxMsgs {
+					if msg.Type == constant.MsgTypeRecvPacket {
+						ibcTx.DcConnectionId = getConnectByRecvPacketEventsNews(val.EventsNew, index)
+					}
 				}
 			}
 			break
 		}
-		//else if len(dcAddrMap) > 0 {
-		//	//失败的recv_packet交易
-		//	if len(val.Signers) > 0 {
-		//		_, ok := dcAddrMap[val.Signers[0]]
-		//		if ok {
-		//			recvTx = val
-		//			break
-		//		}
-		//	}
-		//}
+
 	}
 	//没有匹配成功，取最新recv_packet
 	if recvTx == nil && len(recvTxs) > 0 {
