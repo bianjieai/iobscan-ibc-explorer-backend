472c1c66c8a843ef8211cbaca8e7a5b3f6cedf8a
longtao <1273211023@qq.com>
2023-04-17 11:43:51 +0800

    adapter uptick

--- internal/app/task/ibc_tx_relate_task_test.go
+++ internal/app/task/ibc_tx_relate_task_test.go
@@ -1,6 +1,7 @@
 package task
 
 import (
+	"fmt"
 	"github.com/bianjieai/iobscan-ibc-explorer-backend/internal/app/utils"
 	"testing"
 )
@@ -25,4 +26,16 @@ func Test_HandlerIbcTxs(t *testing.T) {
 	rw := newIbcTxRelateWorker("relate", "worker", ibcTxTargetLatest, chainMap)
 	rw.handlerIbcTxs(chain, ibcTxList, denomMap)
 	t.Log(utils.MustMarshalJsonToStr(ibcTxList))
+}
+
+func Test_parseRecvPacketTxEvents(t *testing.T) {
+	txs, err := txRepo.GetTxByHashes("uptick", []string{"C476E603D7A3329FCB8486897B465815F10E3A6B70F7BC89657986409CBC3FB6"})
+	if err != nil {
+		t.Log(err.Error())
+	}
+
+	dcConnection, packetAck, exists := parseRecvPacketTxEvents(1, txs[0])
+	fmt.Println(dcConnection)
+	fmt.Println(packetAck)
+	fmt.Println(exists)
 }
