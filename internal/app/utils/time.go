4922ba7e06531e57f71f87cdcecd467d784e31a5
huangweichang <weichang@bianjie.ai>
2023-12-29 20:04:55 +0800

    fix timer goroutine exit

--- internal/app/utils/time.go
+++ internal/app/utils/time.go
@@ -1,19 +1,32 @@
 package utils
 
-import "time"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
 func RunTimer(num int, uint Unit, fn func()) {
 	go func() {
+		sigChan := make(chan os.Signal, 1)
 		// run once right now
+		// 捕获指定的系统信号
+		signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+		t := time.NewTimer(time.Second * 1)
 		fn()
 		for {
 			now := time.Now()
 			next := now.Add(ParseDuration(num, uint))
 			next = TruncateTime(next, uint)
-			t := time.NewTimer(next.Sub(now))
+			t.Reset(next.Sub(now))
 			select {
 			case <-t.C:
 				fn()
+			case <-sigChan:
+				fmt.Println("timer is exist...")
+				return
 			}
 		}
 	}()
